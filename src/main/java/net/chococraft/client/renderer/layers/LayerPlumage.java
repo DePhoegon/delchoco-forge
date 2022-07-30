0353e24ba1b07a326617bdd9c2e588427760b411
Kerns Noel <45953503+DePhoegon@users.noreply.github.com>
2022-07-29 21:28:55 -0600

    Plumage Logic
    Progress file 7-29_post 0-8-5.txt

--- src/main/java/net/chococraft/client/renderer/layers/LayerPlumage.java
+++ src/main/java/net/chococraft/client/renderer/layers/LayerPlumage.java
@@ -4,15 +4,13 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.chococraft.Chococraft;
 import net.chococraft.client.models.entities.AdultChocoboModel;
 import net.chococraft.common.entities.ChocoboEntity;
+import net.chococraft.common.entities.properties.ChocoboColor;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.resources.ResourceLocation;
 
 public class LayerPlumage extends RenderLayer<ChocoboEntity, AdultChocoboModel<ChocoboEntity>> {
-
-	private ResourceLocation PLUMAGE = new ResourceLocation(Chococraft.MOD_ID,"textures/entities/chocobos/plumage.png");
-	//Insert ResourceLocations for Plumable Recolored for different colors
 
 	public LayerPlumage(RenderLayerParent<ChocoboEntity, AdultChocoboModel<ChocoboEntity>> rendererIn) {
 		super(rendererIn);
@@ -22,6 +20,20 @@ public class LayerPlumage extends RenderLayer<ChocoboEntity, AdultChocoboModel<C
 	@Override
 	public void render(PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn, ChocoboEntity chocoboEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
 		if (chocoboEntity.isTame() && !chocoboEntity.isInvisible()) {
+			String coloredPlumage = "textures/entities/chocobos/plumage.png";
+			ChocoboColor color = chocoboEntity.getChocoboColor();
+			switch (color) {
+				case GREEN -> coloredPlumage = "textures/entities/chocobos/plumage_green.png";
+				case BLUE -> coloredPlumage = "textures/entities/chocobos/plumage.png";
+				case WHITE -> coloredPlumage = "textures/entities/chocobos/plumage.png";
+				case BLACK -> coloredPlumage = "textures/entities/chocobos/plumage.png";
+				case GOLD -> coloredPlumage = "textures/entities/chocobos/plumage.png";
+				case PINK -> coloredPlumage = "textures/entities/chocobos/plumage_pink.png";
+				case RED -> coloredPlumage = "textures/entities/chocobos/plumage.png";
+				case PURPLE -> coloredPlumage = "textures/entities/chocobos/plumage.png";
+				case FLAME -> coloredPlumage = "textures/entities/chocobos/plumage.png";
+			}
+			ResourceLocation PLUMAGE = new ResourceLocation(Chococraft.MOD_ID, coloredPlumage);
 			renderColoredCutoutModel(this.getParentModel(), PLUMAGE, matrixStackIn, bufferIn, packedLightIn, chocoboEntity, 1.0F, 1.0F, 1.0F);
 		}
 	}
