7ad7f96940c2110b5b5da12b989f3dc30088fd9f
Kerns Noel (Acer) <45953503+DePhoegon@users.noreply.github.com>
2022-09-14 22:10:05 -0600

    gysahl green plant
    gysahl greens plant check

--- src/main/java/com/dephoegon/delchoco/common/blocks/GysahlGreenBlock.java
+++ src/main/java/com/dephoegon/delchoco/common/blocks/GysahlGreenBlock.java
@@ -1,16 +1,27 @@
 package com.dephoegon.delchoco.common.blocks;
 
+import com.dephoegon.delbase.aid.block.colorshift.grav.sandBlock;
+import com.dephoegon.delbase.aid.block.colorshift.grav.solidSandBlock;
+import com.dephoegon.delbase.aid.block.colorshift.slab.sandSlab;
+import com.dephoegon.delbase.aid.block.colorshift.slab.sandSlabEnergy;
 import com.dephoegon.delchoco.common.init.ModRegistry;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.block.state.properties.SlabType;
+import net.minecraft.world.level.material.FluidState;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+import static net.minecraft.world.level.block.SlabBlock.TYPE;
 
 public class GysahlGreenBlock extends CropBlock {
     public static final int MAX_AGE = 4;
@@ -29,13 +40,59 @@ public class GysahlGreenBlock extends CropBlock {
 
     @Override
     public boolean canSurvive(BlockState state, LevelReader worldIn, BlockPos pos) {
-        return (worldIn.getRawBrightness(pos, 0) >= 8 || worldIn.canSeeSky(pos)) && ((state.getBlock() == this || state.getValue(AGE) == MAX_AGE) &&
+        return (ableSurvive(worldIn, pos, 3)) && ((state.getBlock() == this || state.getValue(AGE) == MAX_AGE) &&
                 super.canSurvive(state, worldIn, pos));
+    }
+    private boolean ableSurvive(@NotNull LevelReader worldIn, BlockPos pos, int lightLevel) {
+        if (worldIn.getRawBrightness(pos, 0) >= lightLevel) return true;
+        if (worldIn.canSeeSky(pos)) return true;
+        BlockState block = worldIn.getBlockState(pos.below());
+        return block.getBlock().defaultBlockState() == Blocks.FARMLAND.defaultBlockState() || ablePlace(block, worldIn, pos.below(), lightLevel);
     }
 
     @Override
     protected boolean mayPlaceOn(BlockState state, BlockGetter worldIn, BlockPos pos) {
-        return super.mayPlaceOn(state, worldIn, pos) || state.is(Blocks.GRASS_BLOCK);
+        return super.mayPlaceOn(state, worldIn, pos) || ablePlace(state, worldIn, pos, 7);
+    }
+    private boolean ablePlace(@NotNull BlockState state, BlockGetter worldIn, BlockPos pos, int light) {
+        // Sand Slabs explictedly allowed because of desert looks & beaches
+        if (state.getBlock() instanceof sandSlab || state.getBlock() instanceof sandSlabEnergy) {
+            SlabType s_type = state.getValue(TYPE);
+            boolean w_logged = state.getValue(SlabBlock.WATERLOGGED);
+            if (s_type != SlabType.BOTTOM) { if (w_logged || checkDirection(worldIn, pos)) { return true; } }
+        }
+        // Sand & Solid stand stated, for the obvious reasons, & Array list used for randomish sorted group of blocks selectively choosen
+        if (state.getBlock() instanceof sandBlock || state.getBlock() instanceof solidSandBlock || blockPlaceableOnList().contains(state.getBlock().defaultBlockState())) { return checkDirection(worldIn, pos); }
+        // phrased this way to keep the old & admittedly personally interesting of planting in grass in daylight of 7 & allowing canSurvive to choose if it pops off or not.
+        return worldIn.getLightEmission(pos) > light && state.is(Blocks.GRASS_BLOCK);
+    }
+    private boolean checkDirection(BlockGetter worldIn , BlockPos pos) {
+        int start = 1;
+        int limit = 4;
+        for (Direction direction : Direction.Plane.HORIZONTAL) {
+            BlockState blockState = worldIn.getBlockState(pos.relative(direction));
+            FluidState fluidState = worldIn.getFluidState(pos.relative(direction));
+            if (fluidState.is(FluidTags.WATER) || blockState.is(Blocks.FROSTED_ICE)) { return true; }
+            if (direction == Direction.SOUTH || direction == Direction.NORTH) {
+                return directionExtension(worldIn, pos, start, limit, direction, null);
+            } else {
+                return directionExtension(worldIn, pos, start, limit, direction, Direction.NORTH) || directionExtension(worldIn, pos, start, limit, direction, Direction.SOUTH);
+            }
+        }
+        return false;
+    }
+    private boolean directionExtension(BlockGetter worldIn, BlockPos pos, int current, int limit, Direction direction, Direction stretch) {
+        BlockPos shiftDirection = pos.relative(direction);
+        if (current <= limit) {
+            BlockState blockState = worldIn.getBlockState(shiftDirection.relative(direction));
+            FluidState fluidState = worldIn.getFluidState(shiftDirection.relative(direction));
+            if (direction == Direction.NORTH || direction == Direction.SOUTH) {
+                return directionExtension(worldIn, shiftDirection, current+1, limit, direction, null) || fluidState.is(FluidTags.WATER) || blockState.is(Blocks.FROSTED_ICE);
+            } else {
+                return directionExtension(worldIn, shiftDirection, current+1, limit, direction, stretch) || directionExtension(worldIn, shiftDirection, 1, limit, stretch, null);
+            }
+        }
+        return false;
     }
 
     @Override
@@ -51,5 +108,14 @@ public class GysahlGreenBlock extends CropBlock {
     @Override
     public int getMaxAge() {
         return MAX_AGE;
+    }
+    private @NotNull ArrayList<Object> blockPlaceableOnList() {
+        ArrayList<Object> block_set = new ArrayList();
+        block_set.add(Blocks.SAND.defaultBlockState());
+        block_set.add(Blocks.RED_SAND.defaultBlockState());
+        block_set.add(Blocks.DIRT.defaultBlockState());
+        block_set.add(Blocks.COARSE_DIRT.defaultBlockState());
+        block_set.add(Blocks.FARMLAND.defaultBlockState());
+        return block_set;
     }
 }
