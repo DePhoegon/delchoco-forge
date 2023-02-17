8c7262361266d57b0c47e2a4b4cd9070565ee834
Kerns Noel (Acer) <45953503+DePhoegon@users.noreply.github.com>
2023-02-17 16:46:38 -0700

    Render Layers
    2-17-23.txt

--- src/main/java/com/dephoegon/delchoco/client/renderer/layers/LayerArmor.java
+++ src/main/java/com/dephoegon/delchoco/client/renderer/layers/LayerArmor.java
@@ -4,17 +4,18 @@ import com.dephoegon.delchoco.DelChoco;
 import com.dephoegon.delchoco.common.entities.Chocobo;
 import com.google.common.collect.Maps;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.Util;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
-import java.util.Objects;
 
 import static com.dephoegon.delchoco.common.init.ModRegistry.*;
 
@@ -25,15 +26,14 @@ public class LayerArmor extends RenderLayer<Chocobo, EntityModel<Chocobo>> {
         map.put(DIAMOND_CHOCO_CHEST.get().getDescriptionId(), new ResourceLocation(DelChoco.MOD_ID, "textures/entities/chocobos/chocobo_armor_diamond.png"));
         map.put(NETHERITE_CHOCO_CHEST.get().getDescriptionId(), new ResourceLocation(DelChoco.MOD_ID, "textures/entities/chocobos/chocobo_armor_netherite.png"));
     });
-    public LayerArmor(RenderLayerParent<Chocobo, EntityModel<Chocobo>> pRenderer) {
-        super(pRenderer);
-    }
+    public LayerArmor(RenderLayerParent<Chocobo, EntityModel<Chocobo>> pRenderer) { super(pRenderer); }
 
     public void render(@NotNull PoseStack matrixStackIn, @NotNull MultiBufferSource bufferIn, int packedLightIn, @NotNull Chocobo chocoboEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         String armor = chocoboEntity.isArmored() ? chocoboEntity.getArmor().getDescriptionId() : null;
-
         if (armor != null && !chocoboEntity.isBaby() && !chocoboEntity.isInvisible()) {
-            renderColoredCutoutModel(this.getParentModel(), CHOCOBO_ARMORS.get(armor), matrixStackIn, bufferIn, packedLightIn, chocoboEntity, 1.0F, 1.0F, 1.0F);
+            VertexConsumer vertexconsumer = bufferIn.getBuffer(RenderType.entityTranslucent(CHOCOBO_ARMORS.get(armor), false));
+            this.getParentModel().renderToBuffer(matrixStackIn, vertexconsumer, packedLightIn, LivingEntityRenderer.getOverlayCoords(chocoboEntity, 0F), 1F, 1F, 1F, .75F);
+            // renderColoredCutoutModel(this.getParentModel(), CHOCOBO_ARMORS.get(armor), matrixStackIn, bufferIn, packedLightIn, chocoboEntity, 1.0F, 1.0F, 1.0F);
         }
     }
 }
--- src/main/java/com/dephoegon/delchoco/client/renderer/layers/LayerPlumage.java
+++ src/main/java/com/dephoegon/delchoco/client/renderer/layers/LayerPlumage.java
@@ -5,9 +5,12 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.dephoegon.delchoco.DelChoco;
 import com.dephoegon.delchoco.common.entities.Chocobo;
 import com.dephoegon.delchoco.common.entities.properties.ChocoboColor;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.Util;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.resources.ResourceLocation;
@@ -37,7 +40,9 @@ public class LayerPlumage extends RenderLayer<Chocobo, EntityModel<Chocobo>> {
 	public void render(@NotNull PoseStack matrixStackIn, @NotNull MultiBufferSource bufferIn, int packedLightIn, @NotNull Chocobo chocobo, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
 		if (chocobo.isTame() && !chocobo.isInvisible() && !chocobo.isBaby()) {
 			ChocoboColor color = chocobo.getChocoboColor();
-			renderColoredCutoutModel(this.getParentModel(), CHOCOBO_PLUMES.get(color), matrixStackIn, bufferIn, packedLightIn, chocobo, 1.0F, 1.0F, 1.0F);
+			VertexConsumer vertexconsumer = bufferIn.getBuffer(RenderType.entityTranslucent(CHOCOBO_PLUMES.get(color), true));
+			this.getParentModel().renderToBuffer(matrixStackIn, vertexconsumer, packedLightIn, LivingEntityRenderer.getOverlayCoords(chocobo, 0F), 1F, 1F, 1F, .65F);
+			// renderColoredCutoutModel(this.getParentModel(), CHOCOBO_PLUMES.get(color), matrixStackIn, bufferIn, packedLightIn, chocobo, 1.0F, 1.0F, 1.0F);
 		}
 	}
 }
