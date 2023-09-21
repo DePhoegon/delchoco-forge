439c0828bb0838962df278d285338fc379a58869
Kerns Noel(Acer) <45953503+DePhoegon@users.noreply.github.com>
2023-09-21 10:32:24 -0600

    Comparisons
    Took 1 hour 29 minutes

--- src/main/java/com/dephoegon/delchoco/common/blocks/StrawNestBlock.java
+++ src/main/java/com/dephoegon/delchoco/common/blocks/StrawNestBlock.java
@@ -25,7 +25,6 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.network.NetworkHooks;
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
